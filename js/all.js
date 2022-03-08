4a72dcbd52e27ef5abb3a09a70e7d856bcf76eb4
joe <joe.chang1014@gmail.com>
2022-03-08 10:38:56 +0800

    refactor all.js

--- js/all.js
+++ js/all.js
@@ -1,57 +1,58 @@
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-var url="https://hexschool.github.io/js-filter-data/data.json"
-var data;
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
+let data = [];
 
-axios.get(url)
- .then(function(res){
-  data=res.data.filter(a=>a.作物名稱)
-  // TODO: 之後拆成 renderData 函式
-  var str=""
-  data.forEach((b,index)=>{
+const renderData = (paramArray) => {
+  let str = '';
+  paramArray.forEach((item) => {
     // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
-    str+=content
-  })
-  table.innerHTML=str
-})
+    const {
+      作物名稱, 市場名稱, 上價, 中價, 下價, 平均價, 交易量,
+    } = item;
+    const content = `<tr>
+        <td> 
+          ${作物名稱}
+        </td>
+        <td> 
+          ${市場名稱}
+        </td>
+        <td> 
+          ${上價}
+        </td>
+        <td> 
+          ${中價}
+        </td>
+        <td> 
+          ${下價}
+        </td>
+        <td> 
+          ${平均價}
+        </td>
+        <td> 
+          ${交易量}
+        </td>
+      </tr>`;
 
-var table=document.querySelector(".table-content")
-var showData=[]
+    str += content;
+  });
+  return str;
+};
 
-var category=""
-var filter=document.querySelector(".filter")
+const table = document.querySelector('.table-content');
+/* global axios */
+axios.get(url).then((res) => {
+  data = res.data.filter((a) => a.作物名稱);
+  // TODO: 之後拆成 renderData 函式
+  table.innerHTML = renderData(data);
+});
 
-filter.addEventListener("click",filterCategory)
+const filterCategory = (e) => {
+  if (e.target.nodeName !== 'BUTTON') return;
+  const { category } = e.target.dataset;
+  const showData = data.filter((i) => i.種類代碼 === category);
+  // TODO: 之後拆成 renderData 函式
+  table.innerHTML = renderData(showData);
+};
 
-function filterCategory(e){
-  if(e.target.nodeName=="BUTTON"){
-    category=e.target.dataset.category
-    showData=data.filter((i)=>{
-      return i.種類代碼==category
-    })
-    // TODO: 之後拆成 renderData 函式
-    var str=""
-    showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
-      str+=content
-    })
-    table.innerHTML=str
-  }else{
-    return;
-  }
-}
-
+const filter = document.querySelector('.filter');
+filter.addEventListener('click', filterCategory);
